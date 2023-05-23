4ddf4d8ab2dbecb027347eee652a54a751f843ab
citec-47 <mauricendonyi40@gmail.com>
2023-05-23 08:04:42 -0700

    Fixing Linterss

--- src/tests/NavBar.test.js
+++ src/tests/NavBar.test.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
